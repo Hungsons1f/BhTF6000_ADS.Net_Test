bcc9d0276b7a53ba817813aed2ae1e4a038a6773
Cydeus <Hungsons1fphu@gmail.com>
2021-01-12 11:28:04 +0700

    First Commit

---
+++ ADS_Form/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ADS_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRead = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tbWrite = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbRead
+            // 
+            this.tbRead.Location = new System.Drawing.Point(12, 12);
+            this.tbRead.Multiline = true;
+            this.tbRead.Name = "tbRead";
+            this.tbRead.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbRead.Size = new System.Drawing.Size(156, 59);
+            this.tbRead.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 77);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(93, 77);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tbWrite
+            // 
+            this.tbWrite.Location = new System.Drawing.Point(12, 106);
+            this.tbWrite.Name = "tbWrite";
+            this.tbWrite.Size = new System.Drawing.Size(156, 20);
+            this.tbWrite.TabIndex = 3;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(12, 133);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 4;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.tbWrite);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.tbRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRead;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox tbWrite;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ ADS_Form/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using TwinCAT.Ads;
+
+namespace ADS_Form
+{
+    public partial class Form1 : Form
+    {
+        private int hValue;
+        private TcAdsClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new TcAdsClient();
+            client.Connect(851);
+
+            try
+            {
+                hValue = client.CreateVariableHandle("MAIN.value");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);             
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                client.DeleteVariableHandle(hValue);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            client.Dispose();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbRead.Text = ((uint)client.ReadAny(hValue, typeof(uint))).ToString();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbRead.Text = null;
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.WriteAny(hValue, Convert.ToUInt16(tbWrite.Text));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
